7aa9a6cd8b3718ca62eb36cc5e27dc6f348799b7
Brad0914 <bradmvf09081999@gmail.com>
2021-08-10 20:25:43 -0600

    Commit final de la aplicación en React

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,82 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {parqueo} from './Parqueo/parqueo'
+import {parser} from './Components/parser'
 
 class App extends React.Component{
+
+  parqueo = new parqueo();
+
   constructor(props){
     super(props);
-    this.state = {apiResponse:""};
+    this.state = {
+      data: [],
+      libres:"",
+      ocupados:"",
+      //apiResponse:""
+    };
   }
 
-  callAPI(){
-    fetch("http://localhost:3001/spaces")
-    .then(res => res.text())
-    .then(res => this.setState({apiResponse: res}));
+  async loadData(){
+    const newData = await this.parqueo.getParqueo();
+    this.setState({
+      data: newData
+    });
   }
 
-  componentWillMount(){
-    this.callAPI();
+  callAPI(url, number){
+    //fetch("http://localhost:3001/spaces/2?infospace=cambie")
+    //.then(res => res.text())
+    //.then(res => this.setState({apiResponse: res}));
+
+
+    if (number === 1){
+      fetch(url)
+      .then(res => res.text())
+      .then(res => this.setState({libres: res}));
+
+    } else if (number === 2){
+      fetch(url)
+      .then(res => res.text())
+      .then(res => this.setState({ocupados: res}));
+    }
+
   }
+
+
+  componentDidMount(){
+    setInterval(() => {
+      this.loadData();
+      this.callAPI("http://localhost:3001/spaces?state=free", 1);
+      this.callAPI("http://localhost:3001/spaces?state=in-use", 2);     
+    });
+
+  }
+
+
 
 
   render(){
     return (
       <div className="App">
+        
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-
+          <p>
+            Campos libres
+          </p>
+          <p>
+            {this.state.libres}
+          </p>
+          <p>
+            Campos ocupados
+          </p>
+          <p>
+            {this.state.ocupados}
+          </p>
         </header>
-        <p>{this.state.apiResponse}</p>
+        <parser datos = {this.state.data}></parser>
       </div>
     );
   }
